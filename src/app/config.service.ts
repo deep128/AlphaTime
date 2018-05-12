f553fb8359bcdc22e93cb9ef7802d882b6237327
Deepak Mishra <deepakm157@gmail.com>
2018-05-12 14:15:31 +0530

    hroku url

--- src/app/config.service.ts
+++ src/app/config.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from "@angular/core";
 @Injectable()
 export class Config {
 
-    public readonly baseAPIUrl:string = "http://localhost:3000/";
+    public readonly baseAPIUrl:string = "https://alphatimeback.herokuapp.com/";
 
 }
